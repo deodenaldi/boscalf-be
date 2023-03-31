7bb206accd17cf7035f8b356f57bc535c837fd04
deodenaldi <deodenaldi@gmail.com>
2023-03-31 10:20:43 +0700

    indexProduct

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -30,15 +30,16 @@
                                             <td> {{ $item->price }} </td>
                                             <td> {{ $item->quantity }} </td>
                                             <td>
-                                                <a href=" {{ route('products.gallery', $item->id) }} "
-                                                    class="btn btn-info btn-sm">
+                                                {{-- <a href=" {{ route('products.gallery', $item->id) }} "
+                                                    class="btn btn-info btn-sm"> --}}
+                                                <a href="#" class="btn btn-info btn-sm">
                                                     <i class="fa fa-picture-o"></i>
                                                 </a>
                                                 <a href="{{ route('products.edit', $item->id) }}"
                                                     class="btn btn-primary btn-sm">
                                                     <i class="fa fa-pencil"></i>
                                                 </a>
-                                                <form action=" {{ route('products.destroy', $item->id) }} " method="post"
+                                                <form action=" {{ route('products.destroy', $item->id) }}" method="post"
                                                     class="d-inline">
                                                     @csrf
                                                     @method('delete')
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'DashboardController@index') -> name('dashboard');
+Route::get('/', 'DashboardController@index')->name('dashboard');
 
 Auth::routes();
 
